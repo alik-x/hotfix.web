ad4acbe568a48440a3e3e8427b7b46c64de6c712
alik.x <alik.x.now@gmail.com>
2020-09-11 13:23:30 +0300

    Updates

---
+++ precache-manifest.51d50db04923152b858f3503795cb80a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc625bf2f673773b9c3fea79bf7c940c",
+    "url": "/hotfix.web/index.html"
+  },
+  {
+    "revision": "e024c8a91e9b31628dac",
+    "url": "/hotfix.web/static/css/main.677b0a34.chunk.css"
+  },
+  {
+    "revision": "4c14fdd5dad8bddd3578",
+    "url": "/hotfix.web/static/js/2.28c9856d.chunk.js"
+  },
+  {
+    "revision": "4e42c7d8b9b18f6b70141efade76003d",
+    "url": "/hotfix.web/static/js/2.28c9856d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e024c8a91e9b31628dac",
+    "url": "/hotfix.web/static/js/main.fb26b0d0.chunk.js"
+  },
+  {
+    "revision": "99fca46330e73d0a6953",
+    "url": "/hotfix.web/static/js/runtime-main.e83ad6de.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/hotfix.web/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/hotfix.web/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/hotfix.web/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/hotfix.web/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/hotfix.web/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/hotfix.web/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/hotfix.web/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/hotfix.web/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/hotfix.web/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/hotfix.web/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/hotfix.web/static/media/sub.4eb05fdd.png"
+  }
+]);
--- src/utils.js
+++
@@ -1,4 +0,0 @@
-export function isEmptyPrice(price) {
-  // eslint-disable-next-line
-  return price == '0' || !price
-}
